63f29339356090bff0844eb7b9f1d9f66714c43b
Domin C <dominikcedro00@gmail.com>
2024-11-13 12:38:54 +0100

    ADD custom validation and more values for models

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
+class OrderStatus(models.TextChoices): # i suppose this is a wat to create enums?
+    NEW = 'NEW', 'New'
+    IN_PROCESS = 'IN_PROCESS', 'In Process'
+    SENT = 'SENT', 'Sent'
+    COMPLETED = 'COMPLETED', 'Completed'
+
+def validate_positive(value): # custom validation for part 5
+    if value <= 0:
+        raise ValidationError(
+            _("%value)s is not positive"),
+            params={"value: value"},
+        )
 class Product(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=100)
-    price = models.FloatField(default=0)
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(decimal_places=2, default=0, validators=[validate_positive])
     available = models.BooleanField(default=True)
 
 class Customer(models.Model):
@@ -16,7 +30,5 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     date = models.DateTimeField()
-    status = models.CharField(max_length=100)
-
-
+    status = models.CharField(max_length=20, choices=OrderStatus.choices)
 
