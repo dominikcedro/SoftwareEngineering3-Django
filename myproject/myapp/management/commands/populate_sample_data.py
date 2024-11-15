e52145d8be101b74407c43bf9959103e3f24212e
Domin C <dominikcedro00@gmail.com>
2024-11-15 16:55:34 +0100

    ADD changes in models to accomodate the new method to populate sample data

---
+++ myproject/myapp/management/commands/populate_sample_data.py
@@ -0,0 +1,28 @@
+# myproject/myapp/management/commands/populate_sample_data.py
+from django.core.management.base import BaseCommand
+from myapp.models import Product, Customer, Order, OrderStatus
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+        Product.objects.all().delete()
+        Customer.objects.all().delete()
+        Order.objects.all().delete()
+
+        product1 = Product.objects.create(
+            name="testing3_product",
+            price=19.99,
+            available=True
+        )
+
+        customer1 = Customer.objects.create(
+            name="testing3_name",
+            address="testing2_address"
+        )
+
+        order1 = Order.objects.create(
+            customer=customer1,
+            status=OrderStatus.NEW
+        )
+
+        order1.products.add(product1)
+        self.stdout.write("Data created successfully.")
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,35 +1,37 @@
+# myproject/myapp/models.py
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
-class OrderStatus(models.TextChoices): # i suppose this is a wat to create enums?
+class OrderStatus(models.TextChoices):
     NEW = 'NEW', 'New'
     IN_PROCESS = 'IN_PROCESS', 'In Process'
     SENT = 'SENT', 'Sent'
     COMPLETED = 'COMPLETED', 'Completed'
 
-def validate_positive(value): # custom validation for part 5
+def validate_positive(value):
     if value <= 0:
         raise ValidationError(
-            _("%value)s is not positive"),
-            params={"value: value"},
+            _("%(value)s is not positive"),
+            params={"value": value},
         )
+
 class Product(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    price = models.DecimalField(decimal_places=2, default=0, validators=[validate_positive])
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=0, validators=[validate_positive])
     available = models.BooleanField(default=True)
 
 class Customer(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
 
 class Order(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
-    date = models.DateTimeField()
+    date = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, choices=OrderStatus.choices)
 
     def calculate_total_price(self):
@@ -37,4 +39,3 @@ class Order(models.Model):
 
     def availability_order(self):
         return all(product.available for product in self.products.all())
-
